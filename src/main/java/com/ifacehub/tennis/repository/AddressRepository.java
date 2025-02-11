082b700ef7e1b85e785405fe2693af40a98cae22
Yasminakhtari <akhtariy85@gmail.com>
2025-02-11 21:56:17 +0530

    minor changes

--- src/main/java/com/ifacehub/tennis/repository/AddressRepository.java
+++ src/main/java/com/ifacehub/tennis/repository/AddressRepository.java
@@ -1,7 +1,9 @@
 package com.ifacehub.tennis.repository;
 import com.ifacehub.tennis.entity.ShippingAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<ShippingAddress,Long>{	
 
 }
--- src/main/java/com/ifacehub/tennis/repository/OtpRepository.java
+++ src/main/java/com/ifacehub/tennis/repository/OtpRepository.java
@@ -6,7 +6,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OtpRepository extends JpaRepository<OTP,String>{
 	List<OTP> findByCreationTimeBefore(LocalDateTime expiry);
 }
